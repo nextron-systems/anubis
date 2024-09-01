407121387de5fd0365345dd05f1d8cd4e174b2df
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-09-01 12:05:56 +0330

    configuring the camera

--- arduino_codes/BLUETOOTH_CAMERA/BLUETOOTH_CAMERA.ino
+++ arduino_codes/BLUETOOTH_CAMERA/BLUETOOTH_CAMERA.ino
@@ -3,7 +3,31 @@
 
 BluetoothSerial serialBT;
 
-
+camera_config_t config = {
+    .pin_pwdn = -1,
+    .pin_reset = -1,
+    .pin_xclk = 0,
+    .pin_sccb_sda = 26,
+    .pin_sccb_scl = 27,
+    .pin_d7 = 35,
+    .pin_d6 = 34,
+    .pin_d5 = 39,
+    .pin_d4 = 36,
+    .pin_d3 = 21,
+    .pin_d2 = 19,
+    .pin_d1 = 18,
+    .pin_d0 = 5,
+    .pin_vsync = 25,
+    .pin_href = 23,
+    .pin_pclk = 22,
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+    .pixel_format = PIXFORMAT_JPEG,
+    .frame_size = FRAMESIZE_SVGA,
+    .jpeg_quality = 12,
+    .fb_count = 2,
+};
 
 void setup() {
   // put your setup code here, to run once:

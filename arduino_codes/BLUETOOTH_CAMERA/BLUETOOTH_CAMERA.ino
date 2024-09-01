8fc6cc01f4af5d242e6f60c33f468011c079e482
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-09-01 12:21:09 +0330

    working on camera captures

--- arduino_codes/BLUETOOTH_CAMERA/BLUETOOTH_CAMERA.ino
+++ arduino_codes/BLUETOOTH_CAMERA/BLUETOOTH_CAMERA.ino
@@ -24,7 +24,7 @@ camera_config_t config = {
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
     .pixel_format = PIXFORMAT_JPEG,
-    .frame_size = FRAMESIZE_SVGA,
+    .frame_size = FRAMESIZE_QVGA,
     .jpeg_quality = 12,
     .fb_count = 2,
 };

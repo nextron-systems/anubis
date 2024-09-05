5658cc9d9fff4818bb655406059457c92d0a52b7
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-09-05 12:15:23 +0330

    camera over wifi

--- arduino_codes/WiFi_CAM/WiFi_CAM.ino
+++ arduino_codes/WiFi_CAM/WiFi_CAM.ino
@@ -1,3 +1,4 @@
+#include "esp_camera.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
 
@@ -5,6 +6,24 @@ const char* ssid = "SSID";
 const char* password = "PASSWORD";
 
 #define CAMERA_MODEL_AI_THINKER
+// AI Thinker ESP32-CAM Pin Mapping
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
 void setup() {
   // put your setup code here, to run once:

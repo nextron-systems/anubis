6a2a30f76e7e7aca6032af2e7330204cbccbcffa
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-09-05 11:58:24 +0330

    SSID and password must be set mannually

--- arduino_codes/WiFi_CONNECTION/WiFi_CONNECTION.ino
+++ arduino_codes/WiFi_CONNECTION/WiFi_CONNECTION.ino
@@ -1,6 +1,9 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
 void setup() {
   // put your setup code here, to run once:
 

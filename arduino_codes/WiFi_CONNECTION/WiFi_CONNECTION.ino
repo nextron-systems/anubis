e4801ab7efa47693b8849b78aaac2227c288070b
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-09-05 12:07:00 +0330

    wip

--- arduino_codes/WiFi_CONNECTION/WiFi_CONNECTION.ino
+++ arduino_codes/WiFi_CONNECTION/WiFi_CONNECTION.ino
@@ -5,19 +5,10 @@ const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_PASSWORD";
 
 #define CAMERA_MODEL_AI_THINKER
-#include "camera_pins.h"
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");Serial.begin(115200);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);

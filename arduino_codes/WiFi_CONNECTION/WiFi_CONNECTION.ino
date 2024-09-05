cf4989ec2099f2eda5197cb0922a98841abaffb0
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-09-05 11:59:53 +0330

    now we're good to go

--- arduino_codes/WiFi_CONNECTION/WiFi_CONNECTION.ino
+++ arduino_codes/WiFi_CONNECTION/WiFi_CONNECTION.ino
@@ -9,6 +9,23 @@ const char* password = "YOUR_PASSWORD";
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 
 }
 

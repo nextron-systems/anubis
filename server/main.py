a1a618952e57fbb1a35ec4d5240bf0c895c95263
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-11-26 02:34:01 +0330

    we need that line as a reference

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ def upload_pic():
         abort(400, 'No image data received')
 
     # Generate a base64-encoded filename
-    base64_filename = base64.urlsafe_b64encode(os.urandom(12)).decode('utf-8') + '.jpg'
+    # base64_filename = base64.urlsafe_b64encode(os.urandom(12)).decode('utf-8') + '.jpg'
 
     # Save the image data to a file
     file_path = os.path.join('.', base64_filename)  # Save in the current directory
